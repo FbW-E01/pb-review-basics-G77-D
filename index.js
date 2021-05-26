30a9a0a62191bb2823fb0a5744eccb3514acee38
Gary Dineen <dineen77@hotmail.com>
2021-05-26 22:16:09 +0200

    done

---
+++ index.js
@@ -0,0 +1,25 @@
+/* 1*/
+const number = 2;
+/*2*/
+const temperature = -1;
+/*3*/
+const stringT = "Helloo"; /* start small */
+/*4*/
+const dog = "tom";
+/*5*/
+const cat = `Test ${dog}`;
+console.log(cat);
+
+const empty = "";
+
+/* 7 8 */
+
+/*9*/
+console.log(typeof Time);
+
+/*10*/
+console.log(typeof Bang, Bang);
+
+/*12*/
+
+console.log(typeof true);
